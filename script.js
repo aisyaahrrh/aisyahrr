01cb06d887ac8b6ea6c68f895c52c1c03973e8dc
aisyaahrrh <122080429+aisyaahrrh@users.noreply.github.com>
2023-01-16 07:58:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+})
+
+document.querySelectorAll('.image-slider img').forEach(images =>{
+    images.onclick = () =>{
+        var src = images.getAttribute('src');
+        document.querySelector('.main-home-image').src = src;
+    };
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop : true,
+    grabCursor: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+    },
+});
